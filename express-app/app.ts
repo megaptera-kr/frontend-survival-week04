2d27181572b8498961299b4e940699fd53224a64
BORA <a52447879@gmail.com>
2023-02-26 10:32:16 +0900

    feat: fix cors error from express-app

--- express-app/app.ts
+++ express-app/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 const port = 3000;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
