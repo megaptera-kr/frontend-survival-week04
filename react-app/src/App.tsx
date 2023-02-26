55465273c7b861654f4868f373096058b4ea6f5b
BORA <a52447879@gmail.com>
2023-02-26 10:35:43 +0900

    feat: update markup in App.tsx

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,36 @@
-export default function App() {
+import Searchbar from './components/Searchbar';
+import './styles.css';
+import { TRestaurantsResponse } from './types/restaurant';
+
+export default function App({ restaurants }: { restaurants: TRestaurantsResponse[] }) {
+  console.log(restaurants);
+
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <div className="food-kiosk">
+      <h1>푸트코트 키오스크</h1>
+
+      <div className="cart">
+        <h2>점심 바구니</h2>
+        <button type="button" className="total-button">
+          합계: 0원 주문
+        </button>
+      </div>
+
+      <Searchbar />
+
+      <table>
+        <thead>
+          <tr>
+            <th>식당 이름</th>
+            <th>종류</th>
+            <th>메뉴</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr />
+        </tbody>
+      </table>
+    </div>
   );
 }
