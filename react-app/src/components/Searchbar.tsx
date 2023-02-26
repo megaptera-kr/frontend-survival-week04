ee54c8abb3ec4eb7b7aaf4e829fb754ca088eda9
BORA <a52447879@gmail.com>
2023-02-26 10:34:51 +0900

    feat: add searchbar components

---
+++ react-app/src/components/Searchbar.tsx
@@ -0,0 +1,10 @@
+function Searchbar() {
+  return (
+    <div className="search-bar">
+      <label htmlFor="food">검색</label>
+      <input type="text" id="food" />
+    </div>
+  );
+}
+
+export default Searchbar;
