439396c4323618d776d6a4f4b99343d01afd36f9
taeck <taeck007@gmail.com>
2023-03-02 15:24:15 +0900

    lint 수정

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,18 @@
-import "./app.css";
-import { useFetch } from "usehooks-ts";
-import SearchBar from "./components/SearchBar";
-import useRestaurants from "./hooks/useRestaurants";
-import FilterCategory from "./components/FilterCategory";
-import { RestaurantType } from "./types/restaurants";
-import Restaurants from "./components/Restaurants";
-import useShoppingBasket from "./hooks/useShoppingBasket";
-import MenuCart from "./components/MenuCart";
-import useReceipt from "./hooks/useReceipt";
-import Receipt from "./components/Receipt";
+import './app.css';
+import { useFetch } from 'usehooks-ts';
+import SearchBar from './components/SearchBar';
+import useRestaurants from './hooks/useRestaurants';
+import FilterCategory from './components/FilterCategory';
+import { RestaurantType } from './types/restaurants';
+import Restaurants from './components/Restaurants';
+import useShoppingBasket from './hooks/useShoppingBasket';
+import MenuCart from './components/MenuCart';
+import useReceipt from './hooks/useReceipt';
+import Receipt from './components/Receipt';
 
 export default function App() {
   const { data: restaurant } = useFetch<{ restaurants: RestaurantType[] }>(
-    "http://localhost:3000/restaurants"
+    'http://localhost:3000/restaurants',
   );
   const {
     restaurantList,
@@ -32,10 +32,10 @@ export default function App() {
 
   const handleOrder = async () => {
     try {
-      fetch("http://localhost:3000/orders", {
-        method: "POST",
+      fetch('http://localhost:3000/orders', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({ menu: menuList, totalPrice }),
       })
