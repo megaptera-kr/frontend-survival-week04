539ffe8da3a4649224efe9372ec00afdb925d033
ilkyeong aum <sunaero@ilkyeongs-MacBook-Air.local>
2023-04-02 01:22:49 +0900

    feat: GET /restaurants api 엔드포인트 추가

--- express-app/app.ts
+++ express-app/app.ts
@@ -1,1 +1,17 @@
-// TODO: Express 를 이용하여 서버를 만들어 주세요.
+import express from 'express';
+import cors from 'cors';
+import restaurants from './mocks/restaurants.json';
+
+const port = 3000;
+const app = express();
+app.use(cors());
+
+app.get('/restaurants', (req, res) => {
+  res.status(200);
+  res.send(restaurants);
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running at http://localhost:${port}`);
+});
