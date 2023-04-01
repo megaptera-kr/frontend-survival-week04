8b0896a44891e88525b0b3f8f36c4f8495d57eef
ilkyeong aum <sunaero@ilkyeongs-MacBook-Air.local>
2023-04-02 02:43:34 +0900

    feat: POST /orders API 엔드포인트 추가

--- express-app/app.ts
+++ express-app/app.ts
@@ -5,10 +5,22 @@ import restaurants from './mocks/restaurants.json';
 const port = 3000;
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.get('/restaurants', (req, res) => {
   res.status(200);
   res.send(restaurants);
+});
+
+app.post('/orders', (req, res) => {
+  const { menu, totalPrice } = req.body;
+
+  res.status(201);
+  res.send({
+    id: Date.now().toString(),
+    menu,
+    totalPrice,
+  });
 });
 
 app.listen(port, () => {
