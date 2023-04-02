e42da4a21c26081ff50eb3fae634af1218f59062
ilkyeong aum <sunaero@ilkyeongs-MacBook-Air.local>
2023-04-02 18:31:49 +0900

    fix: 앱 컴포넌트 테스트 통과를 위해 모킹 로직 추가

--- react-app/src/App.test.tsx
+++ react-app/src/App.test.tsx
@@ -2,6 +2,11 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
+jest.mock('@tanstack/react-query', () => ({
+  useQuery: () => ({ isLoading: false, error: {}, data: [] }),
+  useMutation: () => ({ isLoading: false, error: {}, data: [] }),
+}));
+
 describe('App ', () => {
   it('renders without crash', () => {
     render(<App />);
