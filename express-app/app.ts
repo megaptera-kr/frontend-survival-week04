291fea5712257c51b256688e1d43871796ead06a
sonsurim <surim014@naver.com>
2023-04-02 14:53:48 +0900

    feat: restaurant server api handler 구현

--- express-app/app.ts
+++ express-app/app.ts
@@ -1,1 +1,23 @@
-// TODO: Express 를 이용하여 서버를 만들어 주세요.
+import express from 'express';
+import cors from 'cors'
+import restaurantsJson from './data/restaurants.json'
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/restaurants', (_, res) => {
+	res.status(200).send({ restaurants: restaurantsJson.restaurants });
+});
+
+app.post('/orders', (req, res) => {
+  const clientOrders = req.body
+
+  res.status(201).send({
+    id: Date.now().toString(),
+    ...clientOrders
+  })
+})
+
+app.listen(3000);
