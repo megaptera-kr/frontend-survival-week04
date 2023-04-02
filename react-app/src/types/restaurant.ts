af3ecc7b6ae7f932e32ab4ff40c0879f57a97610
sonsurim <surim014@naver.com>
2023-04-02 22:01:52 +0900

    feat: 글로벌 타입 및 레스토랑 관련 타입 정의

---
+++ react-app/src/types/restaurant.ts
@@ -0,0 +1,29 @@
+export type Menu = {
+  id: string,
+  name: string,
+  price: number
+}
+
+export type Category = '전체' |'중식' | '한식'| '일식';
+
+export type Restaurant = {
+  id: string
+  category: Category
+  name: string
+  menu: Menu[]
+}
+
+export type Restaurants = {
+  restaurants: Restaurant[]
+}
+
+export type CartItem = {
+  menu: Menu[],
+  totalPrice: number
+}
+
+export type Receipt = {
+  id: string,
+  menu: Menu[],
+  totalPrice: number
+}
