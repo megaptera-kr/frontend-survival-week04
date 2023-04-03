bd77d678c37e50bb16ae76030ad479918eddd55e
StarryPro <hansunguk6178@gmail.com>
2023-04-03 15:20:54 +0900

    feat: App 및 main 파일

---
+++ react-app/src/hooks/useFetchRestaurants.ts
@@ -0,0 +1,15 @@
+import { useFetch } from 'usehooks-ts';
+
+import Restaurant from '../types/Restaurant';
+
+const url = 'http://localhost:3000/restaurants';
+
+interface Restaurants {
+  [restaurant: string]: Restaurant[]
+}
+
+export default function useFetchRestaurants() {
+  const { data } = useFetch<Restaurants>(url);
+  if (!data) { return []; }
+  return data.restaurants;
+}
