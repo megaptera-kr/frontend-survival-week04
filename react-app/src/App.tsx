373d6bdc607c55f4a58c966cdcdc77378d444c5d
희지 <heez@huijiui-MacBookPro.local>
2023-04-09 21:40:59 +0900

    키오스크 구현

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,45 @@
+import { useEffect } from 'react';
+import { useBoolean, useInterval, useLocalStorage } from 'usehooks-ts';
+
+import Cart from './components/Cart';
+
+import Receipt from './types/Receipt';
+
+import useFetchRestaurants from './hooks/useFetchRestaurants';
+import FilteredRestaurantTable from './components/FilteredRestaurantTable';
+import ReceiptPrinter from './components/ReceiptPrinter';
+
+const emptyReceipt = {} as Receipt;
+
 export default function App() {
+  const { value, setTrue, setFalse } = useBoolean(false);
+  const [receipt, setReceipt] = useLocalStorage('receipt', emptyReceipt);
+  const restaurants = useFetchRestaurants();
+
+  useEffect(() => {
+    setFalse();
+
+    if (receipt.id) {
+      setTrue();
+    }
+  }, [receipt]);
+
+  useInterval(() => {
+    if (receipt.id) {
+      setReceipt(emptyReceipt);
+      setFalse();
+    }
+  }, value ? 5000 : null);
+
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <>
+      <h1>푸드코트 키오스크</h1>
+      {/* 장바구니 */}
+      <Cart setReceipt={setReceipt} />
+      {/* 키오스크 (식당/메뉴) */}
+      <FilteredRestaurantTable restaurants={restaurants} />
+      {/* 영수증 */}
+      <ReceiptPrinter receipt={receipt} />
+    </>
   );
 }
