24af85c4316501cde51ff1a86547e948b32cc9f2
kaiphojor <jihoopark7666@gmail.com>
2023-05-07 07:39:26 +0900

    feat : 분류 필터

--- react-app/src/components/CategoryButton.tsx
+++ react-app/src/components/CategoryButton.tsx
@@ -1,11 +1,25 @@
+import { MouseEvent } from 'react';
+
 type CategoryButtonProps ={
   category: string;
+  setCategory: (value : string)=> void;
+
 }
 
 export default function CategoryButton({
   category,
+  setCategory,
 } : CategoryButtonProps) {
+  const handleClick = (event : MouseEvent<HTMLButtonElement>) => (
+    setCategory(event.currentTarget.value)
+  );
   return (
-    <button type="button">{category}</button>
+    <button
+      type="button"
+      onClick={handleClick}
+      value={category}
+    >
+      {category}
+    </button>
   );
 }
--- react-app/src/components/FilterSearch.tsx
+++ react-app/src/components/FilterSearch.tsx
@@ -1,9 +1,28 @@
-export default function filterSearch() {
+import { ChangeEvent } from 'react';
+
+type FilterSearchProps = {
+  keyword: string;
+  setKeyword: (value : string)=> void;
+}
+
+export default function filterSearch({
+  keyword,
+  setKeyword,
+}: FilterSearchProps) {
+  const handleTextChange = (event: ChangeEvent<HTMLInputElement>) => (
+    setKeyword(event.target.value)
+  );
+
   return (
 
     <div>
       <label htmlFor="filterSearch">검색</label>
-      <input id="filterSearch" type="text" placeholder="식당 이름" />
+      <input
+        id="filterSearch"
+        type="text"
+        placeholder="식당 이름"
+        onChange={handleTextChange}
+      />
     </div>
   );
 }
