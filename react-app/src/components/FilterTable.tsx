39de6e1e4ca1b328216d1866d412ab6968aa9d64
kaiphojor <jihoopark7666@gmail.com>
2023-05-08 04:54:05 +0900

    chore : isomorphic-fetch package.json 에서 devDependencies ->dependencies로  이동

--- react-app/src/components/FilterTable.tsx
+++ react-app/src/components/FilterTable.tsx
@@ -2,22 +2,17 @@ import FilterCategory from './FilterCategory';
 import FilterSearch from './FilterSearch';
 
 type FilterTableProps = {
-  keyword: string;
   setKeyword: (value : string)=> void;
-  category: string;
   setCategory: (value : string)=> void;
 }
 
 export default function FilterTable({
-  keyword,
   setKeyword,
-  category,
   setCategory,
 }: FilterTableProps) {
   return (
     <div>
       <FilterSearch
-        keyword={keyword}
         setKeyword={setKeyword}
       />
       <FilterCategory
