d30b55a7f8f620307d170bb595ae2e8373e2085e
kaiphojor <jihoopark7666@gmail.com>
2023-05-08 17:52:11 +0900

    fix : node-fetch 변경

--- react-app/src/utils/fetchUtils.ts
+++ react-app/src/utils/fetchUtils.ts
@@ -1,6 +1,6 @@
 export default function fetchUtils() {
   // domain + port
-  const authority = 'http://127.0.0.1:3000';
+  const authority = 'http://127.0.0.1:3000'; // local에서는 locahost
   return {
     authority,
   };
