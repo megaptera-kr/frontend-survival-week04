132f1d9983dfba32944edddb017dd5d8a4d8f458
bereal1995 <gygud98@gmail.com>
2023-08-13 19:49:32 +0900

    feat: 카테고리 중복 제거

--- react-app/utils/selectCategories.ts
+++ react-app/utils/selectCategories.ts
@@ -3,6 +3,13 @@ import { IRestaurant } from '../src/types/restaurants';
 import DEFAULT_SELECTED_CATEGORY from '../src/constants/categories';
 
 export default function selectCategories(restaurants: IRestaurant[]) {
-  const categories = restaurants.map((restaurant) => restaurant.category);
-  return [DEFAULT_SELECTED_CATEGORY, ...categories];
+  return restaurants
+    .reduce<string[]>((acc, restaurant) => {
+      const { category } = restaurant;
+      if (!acc.includes(category)) {
+        return [...acc, category];
+      }
+
+      return acc;
+    }, [DEFAULT_SELECTED_CATEGORY]);
 }
