451ebd906c91186a71a7d1bffec45a509a32c1c8
jaehunn <qkdwogns98@gmail.com>
2023-08-13 14:47:41 +0900

    feat: 식당 목록 UI

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,5 +1,14 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
 function main() {
-  // TODO: App 컴포넌트를 render 해주세요.
+  const element = document.getElementById('root');
+
+  if (!element) return;
+
+  const rootElement = ReactDOM.createRoot(element);
+
+  rootElement.render(<App />);
 }
 
 main();
