964d97fc3bd0ee2de5477c9a844a244b8a6b3f68
shcha <>
2023-10-15 23:46:45 +0900

    라이브러리는 다 설치한 것 같은데, 에러가 나서 진행을 못함

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,36 @@
+import { useInterval, useLocalStorage } from 'usehooks-ts';
+// import useSelectMenu from './hook/useSelectMenu';
+// import SelectMenuList from './components/SelectMenuList';
+// import FilterableRestaurantTable from './components/FilterableRestaurantTable';
+
+// import useFetchRestaurant from './hook/useFetchRestaurant';
+// import ReceiptList from './components/ReceiptList';
+
 export default function App() {
+
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <div>
+      <h1>푸드코트 키오스크</h1>
+      
+      <div>
+        <h2>점심 바구니</h2>
+      </div>
+
+      <div>
+        <div>검색</div>
+        <div>
+          <table>
+            <thead>
+              <th>식당 이름</th>
+              <th>종류</th>
+              <th>메뉴</th>
+            </thead>
+            <tbody>
+              <td>식당명1</td>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   );
 }
--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,5 +1,17 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import App from "./App";
+
 function main() {
   // TODO: App 컴포넌트를 render 해주세요.
+  const container = document.getElementById('root');
+  if (!container) {
+    return;
+  }
+
+  const root = ReactDOM.createRoot(container);
+  root.render(React.createElement(App, null));
+  
 }
 
 main();
