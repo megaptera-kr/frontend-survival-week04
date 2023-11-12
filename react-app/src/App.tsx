713483a4cd87954a28ac7bfd7c6c1d50df26d09a
headwing <cdw9844@gmail.com>
2023-11-12 23:26:25 +0900

    파일 추가

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,32 @@
+import { useInterval, useLocalStorage } from 'usehooks-ts';
+
+import Cart from './components/Cart';
+import FilterableRestaurantTable from './components/FilterableRestaurantTable';
+import ReceiptPrinter from './components/ReceiptPrinter';
+
+import useFetchRestaurants from './hooks/useFetchRestaurants';
+
+import Receipt from './types/Receipt';
+
+const emptyReceipt = {} as Receipt;
+
 export default function App() {
+  const [receipt, setReceipt] = useLocalStorage('receipt', emptyReceipt);
+
+  const restaurants = useFetchRestaurants();
+
+  useInterval(() => {
+    if (receipt.id) {
+      setReceipt(emptyReceipt);
+    }
+  }, receipt.id ? 5000 : null);
+
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <div>
+      <h1>푸드코트 키오스크</h1>
+      <Cart setReceipt={setReceipt} />
+      <FilterableRestaurantTable restaurants={restaurants} />
+      <ReceiptPrinter receipt={receipt} />
+    </div>
   );
 }
