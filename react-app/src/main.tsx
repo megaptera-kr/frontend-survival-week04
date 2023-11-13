0061615dc9b1b1c2fd22f84fb1d440439634528c
김예림 <61038091+Kimyerim1935@users.noreply.github.com>
2023-11-13 20:32:07 +0900

    mod: css module로 변경

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './main.css';
+import './main.module.css';
 
 async function main() {
   const rootElement = document.getElementById('root');
