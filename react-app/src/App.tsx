61a8ee447a4911f9c3f0e944ab50cb7f93134d6a
hsshin <hsshin@saltlux.com>
2023-12-08 17:56:33 +0900

    [feat] app 작성중 중간저장

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,48 @@
+import { useFetch } from 'usehooks-ts';
+
 export default function App() {
+  const { data, error } = useFetch('http://localhost:3000/restaurants');
+  console.log(data);
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <>
+      <h1>푸드코트 키오스크</h1>
+      {/* 주문서 파트 */}
+      <div style={{ marginBottom: '3rem' }}>
+        <h2>점심 바구니</h2>
+        {/* <p>뭔가 있을거임</p> */}
+        <button>합계: 0원 주문</button>
+      </div>
+      {/* 검색 파트 */}
+      <div>
+        <label style={{ paddingRight: '1rem' }}>검색</label>
+        <input type="text" placeholder="식당 이름" />
+        <ul style={{ display: 'flex', padding: '0px', listStyle: 'none' }}>
+          <li style={{ marginRight: '1rem' }}>
+            <button type="button">전체</button>
+          </li>
+          <li style={{ marginRight: '1rem' }}>
+            <button type="button">한식</button>
+          </li>
+          <li style={{ marginRight: '1rem' }}>
+            <button type="button">일식</button>
+          </li>
+          <li style={{ marginRight: '1rem' }}>
+            <button type="button">중식</button>
+          </li>
+        </ul>
+      </div>
+      {/* 테이블 파트 */}
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th style={{ paddingInline: '2rem' }}>식당 이름</th>
+              <th>종류</th>
+              <th>메뉴</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </>
   );
 }
