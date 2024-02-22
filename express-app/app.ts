391250a62baf578ac87f43698e3c05ba5201f511
swd <tladnjsen@gmail.com>
2024-02-22 22:49:11 +0900

    [Backend] Add initial code for app.ts

--- express-app/app.ts
+++ express-app/app.ts
@@ -1,1 +1,12 @@
-// TODO: Express 를 이용하여 서버를 만들어 주세요.
+import express from 'express';
+import cors from 'cors';
+
+const port = 3000;
+const app = express();
+
+app.use(cors());
+app.get('/', (req, res) => res.send('Hello, World!'));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
