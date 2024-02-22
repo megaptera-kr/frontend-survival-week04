6175f6205c0d0c54e67519245db48e1a8b3a6c36
magrowing <kmagrowing@gmail.com>
2024-02-22 22:12:34 +0900

    4주차 장바구니 구현 중

--- react-app/src/components/RestaurantRow.tsx
+++ react-app/src/components/RestaurantRow.tsx
@@ -1,18 +1,23 @@
-import Food from './Food';
+import Foods from './Foods';
 
 import Restaurant from '../Types/Restaurant';
+import Food from '../Types/Food';
 
 type RestaurantRowProps = {
   restaurant: Restaurant;
+  setChoiceFoods: (food: Food) => void;
 };
 
-export default function RestaurantRow({ restaurant }: RestaurantRowProps) {
+export default function RestaurantRow({
+  restaurant,
+  setChoiceFoods,
+}: RestaurantRowProps) {
   return (
     <tr>
       <td>{restaurant.name}</td>
       <td>{restaurant.category}</td>
       <td>
-        <Food menu={restaurant.menu} />
+        <Foods menu={restaurant.menu} setChoiceFoods={setChoiceFoods} />
       </td>
     </tr>
   );
--- react-app/src/components/TextField.tsx
+++ react-app/src/components/TextField.tsx
@@ -1,8 +1,28 @@
-export default function TextField() {
+import React from 'react';
+
+type TextFieldProps = {
+  filterText: string;
+  setFilterText: (value: string) => void;
+};
+
+export default function TextField({
+  filterText,
+  setFilterText,
+}: TextFieldProps) {
+  const handelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setFilterText(value);
+  };
   return (
     <div className="TextField">
       <label htmlFor="search">검색</label>
-      <input type="text" placeholder="식당이름" id="search" />
+      <input
+        type="text"
+        placeholder="식당이름"
+        id="search"
+        value={filterText}
+        onChange={handelChange}
+      />
     </div>
   );
 }
