49407805e52b17ec92d0dda0ded3a62e81f2dc53
swd <tladnjsen@gmail.com>
2024-02-23 09:59:07 +0900

    [Frontend] Trivial

--- react-app/src/component/OrderBox.tsx
+++ react-app/src/component/OrderBox.tsx
@@ -6,7 +6,7 @@ function OrderBox() {
         <span>짜장면(8,000원)</span>
         <button type='button'>취소</button>
       </div>
-      <button>합계: 0원 주문</button>
+      <button type='button'>합계: 0원 주문</button>
     </div>
   );
 }
