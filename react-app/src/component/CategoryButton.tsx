eea5d6b2f9fe1eebc91f9adf7a0e33d0c5b971aa
swd <tladnjsen@gmail.com>
2024-02-23 15:49:35 +0900

    [Frontend] Implement filter restaurants by category button

--- react-app/src/component/CategoryButton.tsx
+++ react-app/src/component/CategoryButton.tsx
@@ -1,13 +1,27 @@
 import Category from '../types/CategoryType';
 
 type CategoryButtonProps = {
-  category: Category | null;
+  category: Category | undefined;
+  handleCategoryName: (value: string) => void;
 };
 
-export default function CategoryButton({ category }: CategoryButtonProps) {
+export default function CategoryButton({
+  category,
+  handleCategoryName,
+}: CategoryButtonProps) {
+  const handleClick = () => {
+    let query = '';
+    if (category) {
+      query = category.name ?? '';
+    }
+    handleCategoryName(query);
+  };
+
   return (
     <li className='category-li'>
-      <button type='button'>{category ? category.name : '전체'}</button>
+      <button type='button' onClick={handleClick}>
+        {category ? category.name : '전체'}
+      </button>
     </li>
   );
 }
