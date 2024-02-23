54d4351ae22fad35dd65c9bcdafc8b18061a924f
swd <tladnjsen@gmail.com>
2024-02-23 14:48:16 +0900

    [Backend] Modify return type of GET categories

--- express-app/src/controller/category-controller.ts
+++ express-app/src/controller/category-controller.ts
@@ -2,15 +2,20 @@ import { Express } from 'express';
 
 import restaurants from '../database/table-restaurant';
 
+import Category from '../types/CategoryType';
+
 function CategoryController(app: Express) {
   app.get('/categories', (req, res) => {
     const uniqueCaterogies = new Set();
     restaurants.forEach((restaurant) => {
       uniqueCaterogies.add(restaurant.category);
     });
-    const categories = Array.from(uniqueCaterogies).map((item, i) => ({
-      [(i + 1).toString()]: item,
-    }));
+    const categories: Category[] = Array.from(uniqueCaterogies).map(
+      (item, i) => ({
+        id: i + 1,
+        name: item as string,
+      }),
+    );
 
     res.send(categories);
   });
