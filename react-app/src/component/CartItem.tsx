03078d99a13c6aa0a4bdcf9a9e4a423277291345
swd <tladnjsen@gmail.com>
2024-02-23 21:22:54 +0900

    [Frontend] Implement remove menu item from the cart

--- react-app/src/component/CartItem.tsx
+++ react-app/src/component/CartItem.tsx
@@ -2,15 +2,32 @@ import RestaurantMenu from '../types/RestaurantMenuType';
 
 type CartItemProps = {
   cartItem: RestaurantMenu;
+  handleUpdateCart: () => void;
 };
 
-export default function CartItem({ cartItem }: CartItemProps) {
+export default function CartItem({
+  cartItem,
+  handleUpdateCart,
+}: CartItemProps) {
+  const handleClick = () => {
+    const cartItems: RestaurantMenu[] = JSON.parse(
+      localStorage.getItem('cart') || '[]',
+    );
+    const updatedCartItems: RestaurantMenu[] = cartItems.filter(
+      (item: RestaurantMenu) => item.id !== cartItem.id,
+    );
+    localStorage.setItem('cart', JSON.stringify(updatedCartItems));
+    handleUpdateCart();
+  };
+
   return (
     <div>
       <span>
         {cartItem.name}({cartItem.price})
       </span>
-      <button type='button'>취소</button>
+      <button type='button' onClick={handleClick}>
+        취소
+      </button>
     </div>
   );
 }
