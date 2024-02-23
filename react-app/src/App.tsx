02e07aa978b88e85911aec8429bd368c05dfd2d5
swd <tladnjsen@gmail.com>
2024-02-23 10:08:08 +0900

    [Frontend] Add component MenuTable

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 
 import OrderBox from './component/OrderBox';
 import MenuSearchBar from './component/MenuSearchBar';
+import MenuTable from './component/MenuTable';
 
 export default function App() {
   return (
@@ -10,59 +11,7 @@ export default function App() {
       <div className='container'>
         <OrderBox />
         <MenuSearchBar />
-
-        <div className='menu-table'>
-          <table>
-            <thead>
-              <th>식당 이름</th>
-              <th>종류</th>
-              <th>메뉴</th>
-            </thead>
-            <tbody>
-              <tr>
-                <td>메가반점</td>
-                <td>중식</td>
-                <td>
-                  <ul>
-                    <li>
-                      <span>짜장면(8,000)</span>
-                      <button type='button'>선택</button>
-                    </li>
-                    <li>
-                      <span>짬뽕(8,000)</span>
-                      <button type='button'>선택</button>
-                    </li>
-                    <li>
-                      <span>차돌짬뽕(9,000원)</span>
-                      <button type='button'>선택</button>
-                    </li>
-                  </ul>
-                </td>
-              </tr>
-
-              <tr>
-                <td>메리김밥</td>
-                <td>한식</td>
-                <td>
-                  <ul>
-                    <li>
-                      <span>김밥(3,500원)</span>
-                      <button type='button'>선택</button>
-                    </li>
-                    <li>
-                      <span>참치김밥(4,500원)</span>
-                      <button type='button'>선택</button>
-                    </li>
-                    <li>
-                      <span>제육김밥(5,000원)</span>
-                      <button type='button'>선택</button>
-                    </li>
-                  </ul>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <MenuTable />
 
         <div>
           <p>[영수증 나오는 곳]</p>
---
+++ react-app/src/component/MenuTable.tsx
@@ -0,0 +1,56 @@
+export default function MenuTable() {
+  return (
+    <div className='menu-table'>
+      <table>
+        <thead>
+          <th>식당 이름</th>
+          <th>종류</th>
+          <th>메뉴</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>메가반점</td>
+            <td>중식</td>
+            <td>
+              <ul>
+                <li>
+                  <span>짜장면(8,000)</span>
+                  <button type='button'>선택</button>
+                </li>
+                <li>
+                  <span>짬뽕(8,000)</span>
+                  <button type='button'>선택</button>
+                </li>
+                <li>
+                  <span>차돌짬뽕(9,000원)</span>
+                  <button type='button'>선택</button>
+                </li>
+              </ul>
+            </td>
+          </tr>
+
+          <tr>
+            <td>메리김밥</td>
+            <td>한식</td>
+            <td>
+              <ul>
+                <li>
+                  <span>김밥(3,500원)</span>
+                  <button type='button'>선택</button>
+                </li>
+                <li>
+                  <span>참치김밥(4,500원)</span>
+                  <button type='button'>선택</button>
+                </li>
+                <li>
+                  <span>제육김밥(5,000원)</span>
+                  <button type='button'>선택</button>
+                </li>
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
