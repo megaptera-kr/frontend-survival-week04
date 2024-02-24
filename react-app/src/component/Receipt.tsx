47403263b643d5c69ddc519514424bdc0cdf9f74
swd <tladnjsen@gmail.com>
2024-02-25 00:15:47 +0900

    [Frontend] Modify Receipt component - Tt's a just modification to cover all test - It should use RestaurantMenu component already exists

--- react-app/src/component/Receipt.tsx
+++ react-app/src/component/Receipt.tsx
@@ -1,5 +1,7 @@
 import { useInterval } from 'usehooks-ts';
 
+import moneyformat from '../utils/common';
+
 import ReceiptType from '../types/ReceiptType';
 
 type ReceiptProps = {
@@ -27,20 +29,33 @@ function Receipt({
       {!isShowReceipt ? (
         <p>[영수증 나오는 곳]</p>
       ) : (
-        <div>
-          <h3>영수증</h3>
-          <p>{receipt.id}</p>
-          <h2>주문목록</h2>
-          <ul>
-            {receipt.menu.map((item) => (
-              <li key={item.id}>
-                {item.name}({item.price}원)
-              </li>
-            ))}
-          </ul>
-          <p>
-            총가격<span>: {receipt.totalPrice}</span>
-          </p>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            textAlign: 'center',
+            width: '30%',
+            border: '1px solid black',
+          }}
+        >
+          <h2>영수증</h2>
+          <div>
+            <h3>주문번호</h3>
+            <p>{receipt.id}</p>
+          </div>
+          <div>
+            <h3>주문목록</h3>
+            <ul style={{ listStyle: 'none', padding: '0px' }}>
+              {receipt.menu.map((item) => (
+                <li key={item.id}>
+                  {item.name}({item.price}원)
+                </li>
+              ))}
+            </ul>
+            <p>
+              총 가격: <span>{moneyformat(receipt.totalPrice)}원</span>
+            </p>
+          </div>
         </div>
       )}
     </div>
