7800a4c9f27b10d8170db60d393180fcd16d46eb
doyunnn <73818291+doyunnn@users.noreply.github.com>
2024-02-25 17:55:03 +0900

    [FIX] Fix bugs

--- react-app/src/hooks/useFetchrestaurants.ts
+++ react-app/src/hooks/useFetchrestaurants.ts
@@ -6,10 +6,14 @@ export default function useFetchrestaurants() {
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
 
   useEffect(() => {
-    const url = 'http://localhost:3000/restaurants';
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setRestaurants(data.restaurants));
+    const fetchProducts = async () => {
+      const url = 'http://localhost:3000/restaurants';
+      const response = await fetch(url);
+      const data = await response.json();
+      setRestaurants(data.products);
+    };
+
+    fetchProducts();
   }, []);
 
   return { restaurants };
