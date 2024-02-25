2c30cf1c3ca409f8840f865a4253ee6a277e67fe
doyunnn <73818291+doyunnn@users.noreply.github.com>
2024-02-25 18:23:16 +0900

    [FIX] Fix bugs

--- react-app/src/components/cart/components/OrderButton.tsx
+++ react-app/src/components/cart/components/OrderButton.tsx
@@ -1,13 +1,13 @@
 import useCartList from '../hooks/useCartList';
-import getTotalPrice from '../utils/totalPrice';
 
 export default function OrderButton() {
   const { cart, handleOrderInCart } = useCartList();
+  const totalPrice = cart.reduce((acc, cur) => acc + cur.price, 0);
   return (
     <button type="button" onClick={() => handleOrderInCart()}>
-      합계 :
+      합계:
       {' '}
-      {getTotalPrice(cart)}
+      {totalPrice.toLocaleString()}
       원 주문
     </button>
   );
